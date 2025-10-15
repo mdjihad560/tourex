08efdac6a3fb3bf87628cbdc19d0857f4ef24e1b
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2025-10-15 08:08:14 +0600

    update

--- js/main.js
+++ js/main.js
@@ -43,51 +43,44 @@ OFF CANVAS JS INIT
     });
 
     // progress top js
+   $(window).on("scroll", function () {
+     var scroll = $(window).scrollTop();
+     if (scroll < 245) {
+       $(".scroll-to-target").removeClass("open");
+     } else {
+       $(".scroll-to-target").addClass("open");
+     }
+   });
 
-    var progressPath = document.querySelector(".progress-wrap path");
-    var pathLength = progressPath.getTotalLength();
-    progressPath.style.transition = progressPath.style.WebkitTransition =
-      "none";
-    progressPath.style.strokeDasharray = pathLength + " " + pathLength;
-    progressPath.style.strokeDashoffset = pathLength;
-    progressPath.getBoundingClientRect();
-    progressPath.style.transition = progressPath.style.WebkitTransition =
-      "stroke-dashoffset 10ms linear";
-    var updateProgress = function () {
-      var scroll = $(window).scrollTop();
-      var height = $(document).height() - $(window).height();
-      var progress = pathLength - (scroll * pathLength) / height;
-      progressPath.style.strokeDashoffset = progress;
-    };
-    updateProgress();
-    $(window).scroll(updateProgress);
-    var offset = 50;
-    var duration = 550;
-    jQuery(window).on("scroll", function () {
-      if (jQuery(this).scrollTop() > offset) {
-        jQuery(".progress-wrap").addClass("active-progress");
-      } else {
-        jQuery(".progress-wrap").removeClass("active-progress");
-      }
-    });
-    jQuery(".progress-wrap").on("click", function (event) {
-      event.preventDefault();
-      jQuery("html, body").animate({ scrollTop: 0 }, duration);
-      return false;
-    });
+   /*===========================================
+	=           Scroll Up  	         =
+=============================================*/
+   if ($(".scroll-to-target").length) {
+     $(".scroll-to-target").on("click", function () {
+       var target = $(this).attr("data-target");
+       // animate
+       $("html, body").animate(
+         {
+           scrollTop: $(target).offset().top,
+         },
+         0
+       );
+     });
+   }
+
   }); /*End document ready*/
 
   $(window).on("resize", function () {}); // end window resize
 
   $(window).on("load", function () {
-    preloader();
+
+    $("#loading").fadeOut(500);
+    
   }); // End window LODE
 
   new WOW().init();
 
-  function preloader() {
-    $(".preloader").delay(0).fadeOut();
-  }
+
 })(jQuery);
 
 
